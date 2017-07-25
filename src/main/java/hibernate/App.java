0160aa88c0f106edf2f2016b4e94810ac33075c1
Pasha <pahaoks@gmail.com>
2017-07-25 17:21:10 +0300

    ffff

--- src/main/java/hibernate/App.java
+++ src/main/java/hibernate/App.java
@@ -11,17 +11,17 @@ public class App {
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
         Stock stock = new Stock();
-        stock.setStockCode("4723");
-        stock.setStockName("GENM71");
+        stock.setStockCode("4726");
+        stock.setStockName("GENM77");
         session.save(stock);
         session.getTransaction().commit();
 
         //List<Stock> list = session.createCriteria(Stock.class).list();
-        List<Stock> list = session.createQuery("from Stock where stockName = 'GENM70'").list();
+        List<Stock> list = session.createQuery("from Stock where stockName = 'GENM75'").list();
         for (Stock s: list) {
             System.out.println(s.stockName + " " + s.stockCode + " " + s.stockId);
         }
 
-        session.close();
+        HibernateUtil.shutdown();
     }
 }
